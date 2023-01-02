475327c292572ccc7f6abb22c8eab401e6353717
남궁혜진 <nkhj0404@gmail.com>
2023-01-02 11:46:05 +0900

    fix: 페이지접근처리 미들웨어> getServerSideProps로 이동

--- front/middleware.ts
+++ front/middleware.ts
@@ -2,11 +2,10 @@ import { PATH } from "@hocs/withGetServerSideProps";
 import { NextRequest, NextResponse } from "next/server";
 
 export default function commonMiddleware(req: NextRequest) {
-  const token = req.cookies.get("userToken");
-
-  if (!token) {
-    return NextResponse.redirect(new URL(PATH.LOGIN, req.nextUrl));
-  }
+  // const token = req.cookies.get("userToken");
+  // if (!token) {
+  //   return NextResponse.redirect(new URL(PATH.LOGIN, req.nextUrl));
+  // }
 }
 
 export const config = {
